35f14c7a9726f9d844a865ba7481f283d16c7cfd
Jatin Bhandari <jbhandari@users.noreply.github.com>
2018-09-14 14:35:34 -0400

    Update AppiumConnect.rb

--- lib/AppiumConnect.rb
+++ lib/AppiumConnect.rb
@@ -24,6 +24,7 @@ end
 input_array = ARGV
 ip = ENV['IP'] || Ip.host_ip
 host_port = ip.include?(':') ? ip.split(':')[-1] : nil
+ip = ip.include?(':') ? ip.split(':')[0] : ip
 hub = '127.0.0.1'
 hub_port = '4444'
 
